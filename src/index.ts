c20076941b667a2bf5ff7881495ac5b331c758cc
s8_smits_v <s8_smits_v@venta.lv>
2020-11-29 03:07:51 +0200

    Export base modules in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
-import fetch from 'node-fetch';
+import User from './user';
 import VideoExtractor from './video/videoExtractor';
+import SubscriptionFeed from './feeds/subscriptionFeed';
 
-(async () => {
-    const url = 'UxxajLWwzqY';
-    new VideoExtractor().extractVideo(url);
-})();
+interface YoutubeOptions {
+    userCookieString: string; 
+}
+
+export default function youtubeUser({ userCookieString }: YoutubeOptions) {
+    const user = new User(userCookieString)
+    const videoExtractor = new VideoExtractor({ user });
+    const subscriptionFeed =  new SubscriptionFeed({ user });
+
+    return {
+        user,
+        videoExtractor,
+        subscriptionFeed
+    }
+
+}
+
+export { 
+    SubscriptionFeed,
+    User,
+    VideoExtractor,
+};
+
